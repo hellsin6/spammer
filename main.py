171b32af5541645976032e9379fe67402f364ad0
Hell <hellsing.otaku@gmail.com>
2021-04-13 22:04:20 -0400

    Añadido agregador
    envío de packet "RequestFriend" dentro del parser de kekos

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 from G_Python.g_python.gextension import Extension
 from G_Python.g_python.htools import RoomUsers
+from G_Python.g_python.hparsers import HEntityType
+from G_Python.g_python.hpacket import HPacket
 
 extension_info = {
     "title": "Room scanner",
@@ -12,5 +14,11 @@ extension_info = {
 ext = Extension(extension_info, sys.argv)
 ext.start()
 
+def spammer(users):
+	for user in users:
+		if user.entity_type == HEntityType.HABBO:
+			ext.send_to_server(HPacket("RequestFriend", user.name))
+			print("Added %s" % user.name)
+
 room_users = RoomUsers(ext)
-room_users.on_new_users(lambda users: print(list(map(str, users))))
+room_users.on_new_users(spammer)
